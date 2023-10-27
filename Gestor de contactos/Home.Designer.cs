bd26871fcf8cf97bf2253323d1f2f5e054d3e19a
josueorellana <jm5482017.com>
2023-10-27 13:19:57 -0300

    XDDDDDDD

--- Gestor de contactos/Home.Designer.cs
+++ Gestor de contactos/Home.Designer.cs
@@ -41,7 +41,7 @@ namespace Gestor_de_contactos
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(232, 150);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(46, 28);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -53,7 +53,7 @@ namespace Gestor_de_contactos
             // 
             this.button1.Location = new System.Drawing.Point(6, 19);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(126, 40);
+            this.button1.Size = new System.Drawing.Size(125, 40);
             this.button1.TabIndex = 1;
             this.button1.Text = "+ Agregar contacto";
             this.button1.UseVisualStyleBackColor = true;
@@ -71,7 +71,7 @@ namespace Gestor_de_contactos
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(257, 12);
+            this.button2.Location = new System.Drawing.Point(260, 12);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(583, 40);
             this.button2.TabIndex = 2;
