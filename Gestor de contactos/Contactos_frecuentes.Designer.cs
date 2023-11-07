0dd1ca3ee9e0837fc02a85289c049b470657a8b0
josueorellana <jm5482017.com>
2023-11-07 18:02:46 -0300

    Xddddd

--- Gestor de contactos/Contactos_frecuentes.Designer.cs
+++ Gestor de contactos/Contactos_frecuentes.Designer.cs
@@ -49,11 +49,12 @@ namespace Gestor_de_contactos
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label1.Location = new System.Drawing.Point(300, 196);
+            this.label1.Location = new System.Drawing.Point(255, 221);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(49, 16);
             this.label1.TabIndex = 1;
             this.label1.Text = "Correo";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Contactos_frecuentes
             // 
--- Gestor de contactos/Contactos_frecuentes.cs
+++ Gestor de contactos/Contactos_frecuentes.cs
@@ -16,5 +16,10 @@ namespace Gestor_de_contactos
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
