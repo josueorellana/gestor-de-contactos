fc8bddbc6b3201d07653bd990adfeb4c5533a3ae
josueorellana <jm5482017.com>
2023-11-10 17:13:02 -0300

    Tabla2

--- Gestor de contactos/MenuPrincipal.Designer.cs
+++ Gestor de contactos/MenuPrincipal.Designer.cs
@@ -272,7 +272,7 @@ namespace Gestor_de_contactos
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(47, 0);
+            this.pictureBox2.Location = new System.Drawing.Point(50, -1);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(107, 101);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
