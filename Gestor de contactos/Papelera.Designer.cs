65dd39d9c2aca75baa01bbddccf834c40b4ea5fc
josueorellana <jm5482017.com>
2023-11-10 16:06:47 -0300

    gggggggggggggg

--- Gestor de contactos/Papelera.Designer.cs
+++ Gestor de contactos/Papelera.Designer.cs
@@ -35,8 +35,8 @@ namespace Gestor_de_contactos
             this.button3 = new System.Windows.Forms.Button();
             this.panelayuda = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.btnvaciar = new System.Windows.Forms.Button();
             this.lblaviso = new System.Windows.Forms.Label();
+            this.btnvaciar = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panelayuda.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -44,7 +44,7 @@ namespace Gestor_de_contactos
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(714, 12);
+            this.button1.Location = new System.Drawing.Point(692, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 22);
             this.button1.TabIndex = 0;
@@ -102,15 +102,6 @@ namespace Gestor_de_contactos
             this.panel2.Size = new System.Drawing.Size(812, 80);
             this.panel2.TabIndex = 3;
             // 
-            // btnvaciar
-            // 
-            this.btnvaciar.Location = new System.Drawing.Point(669, 49);
-            this.btnvaciar.Name = "btnvaciar";
-            this.btnvaciar.Size = new System.Drawing.Size(131, 28);
-            this.btnvaciar.TabIndex = 1;
-            this.btnvaciar.Text = "Vaciar Papelera";
-            this.btnvaciar.UseVisualStyleBackColor = true;
-            // 
             // lblaviso
             // 
             this.lblaviso.AutoSize = true;
@@ -123,6 +114,15 @@ namespace Gestor_de_contactos
             this.lblaviso.TabIndex = 2;
             this.lblaviso.Text = "Se eliminaran definitivamente los contactos que hayan estado en la papelera por m" +
     "ás de 30 días ";
+            // 
+            // btnvaciar
+            // 
+            this.btnvaciar.Location = new System.Drawing.Point(669, 49);
+            this.btnvaciar.Name = "btnvaciar";
+            this.btnvaciar.Size = new System.Drawing.Size(131, 28);
+            this.btnvaciar.TabIndex = 1;
+            this.btnvaciar.Text = "Vaciar Papelera";
+            this.btnvaciar.UseVisualStyleBackColor = true;
             // 
             // Papelera
             // 
