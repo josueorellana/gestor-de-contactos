2a83408b500248c1df7bf2b902cafb7a39863146
WilbertGironpw <141753241+WilbertGironpw@users.noreply.github.com>
2023-11-10 11:33:21 -0600

    Add files via upload

---
+++ dbo.Table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Usuarios] (
+    [Iddeusuario]               INT          IDENTITY (1, 1) NOT NULL DEFAULT 0,
+    [Nombres]          CHAR (25)    NOT NULL,
+    [Apellidos]        CHAR (25)    NOT NULL,
+    [Correo]           CHAR (40)    NOT NULL,
+    [Numerodetelefono] NUMERIC (30) NOT NULL,
+    [Password]         CHAR (30)    NOT NULL,
+    PRIMARY KEY CLUSTERED ([Iddeusuario] ASC)
+);
+
