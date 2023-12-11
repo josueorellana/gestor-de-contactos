5fe746dd892fb976bcb11b287ff599e737888747
josueorellana <jm5482017.com>
2023-12-11 07:20:08 -0300

    DataGridView

--- Gestor de contactos/MenuPrincipal.Designer.cs
+++ Gestor de contactos/MenuPrincipal.Designer.cs
@@ -49,6 +49,7 @@ namespace Gestor_de_contactos
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btnbuscarayuda = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.panelSideMenuLateral.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.ptbcerrarsesion)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pibhome)).BeginInit();
@@ -58,9 +59,11 @@ namespace Gestor_de_contactos
             ((System.ComponentModel.ISupportInitialize)(this.ptbnewcontacto)).BeginInit();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panelContenedor.SuspendLayout();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // panelSideMenuLateral
@@ -268,6 +271,7 @@ namespace Gestor_de_contactos
             // panelContenedor
             // 
             this.panelContenedor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
+            this.panelContenedor.Controls.Add(this.dataGridView1);
             this.panelContenedor.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelContenedor.Location = new System.Drawing.Point(210, 0);
             this.panelContenedor.Name = "panelContenedor";
@@ -320,6 +324,14 @@ namespace Gestor_de_contactos
             this.btnbuscarayuda.Text = "buscar";
             this.btnbuscarayuda.UseVisualStyleBackColor = true;
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 84);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(874, 368);
+            this.dataGridView1.TabIndex = 0;
+            // 
             // MenuPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -342,9 +354,11 @@ namespace Gestor_de_contactos
             ((System.ComponentModel.ISupportInitialize)(this.ptbnewcontacto)).EndInit();
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panelContenedor.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -369,5 +383,6 @@ namespace Gestor_de_contactos
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button btnbuscarayuda;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
